52adf741649dd2d75937ff07a0ef6a1e9013ccfb
mnpsnuwan <nuwanp124@gmail.com>
2024-08-15 21:02:46 -0700

    IM:DEV:Microservices Shop Frontend - Implementation of frontend with product and order service

---
+++ src/app/config/auth.config.ts
@@ -0,0 +1,15 @@
+import { PassedInitialConfig } from 'angular-auth-oidc-client';
+
+export const authConfig: PassedInitialConfig = {
+  config: {
+    authority: 'http://localhost:8181/realms/spring-microservices-security-relms',
+    redirectUrl: window.location.origin,
+    postLogoutRedirectUri: window.location.origin,
+    clientId: 'angular-client',
+    scope: 'openid profile offline_access',
+    responseType: 'code',
+    silentRenew: true,
+    useRefreshToken: true,
+    renewTimeBeforeTokenExpiresInSeconds: 30,
+  }
+}
